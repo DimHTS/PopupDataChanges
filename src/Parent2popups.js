993c06dbd887df6bbb9b5bd090e7db1ca5058006
Dim Hutsel <Dreamtm@yandex.ru>
2018-04-27 20:56:22 +0700

    add new parent for 2 popups in 1 parent component

---
+++ src/Parent2popups.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+
+//Component
+import PopupDataChanges from './PopupDataChanges'
+
+class Parent extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isPopupDataChanges_edit: false,
+      isPopupDataChanges_delete: false,
+    }
+  }
+  
+  onChangeData_edit() => {
+    // ...
+    this.setState({ 
+      isPopupDataChanges_delete: false,
+      isPopupDataChanges_edit: true 
+    })
+  }
+  
+  onChangeData_delete() => {
+    // ...
+    this.setState({ 
+      isPopupDataChanges_edit: false,
+      isPopupDataChanges_delete: true
+    })
+  }
+  
+  handleClose__PopupDataChanges = () => {
+    this.setState({ 
+      isPopupDataChanges_edit: false,
+      isPopupDataChanges_delete: false,
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <PopupDataChanges
+          isActive={this.state.isPopupDataChanges_edit}
+          typeDataChanges='edit'
+          handleClosePopup={this.handleClose__PopupDataChanges} />
+          
+        <PopupDataChanges
+          isActive={this.state.isPopupDataChanges_delete}
+          typeDataChanges='delete'
+          handleClosePopup={this.handleClose__PopupDataChanges} />
+      </div>
+    )
+  }
+}
+
+
+export default Parent;
